3caea1afa381bf7b114fd5a9210387488ae6edaf
Omri <mosacho7@gmail.com>
2023-06-04 16:49:34 +0300

    last for rotem

---
+++ Exercises Classification/Ex_Classification_Methoods.py
@@ -0,0 +1,24 @@
+import mediapipe as mp
+import cv2
+import numpy as np
+import pandas as pd
+from  xgboost import XGBClassifier
+import os
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+
+def calculate_angle(a,b,c):
+    a = np.array(a) # First
+    b = np.array(b) # Mid
+    c = np.array(c) # End
+    
+    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+    angle = np.abs(radians*180.0/np.pi)
+    
+    if angle >180.0:
+        angle = 360-angle
+        
+    return angle
+
+
